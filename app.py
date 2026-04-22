002518d56db4acdcb21edf6ea1d9ea897a35a1c5
Angelo Brito <angelobrito500@gmail.com>
2026-04-22 05:03:37 -0400

    fix: add www domain to CORS origins

--- app.py
+++ app.py
@@ -391,7 +391,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174", 
         "http://127.0.0.1:5174",
-        "https://mealfitrd.com"
+        "https://mealfitrd.com",
+        "https://www.mealfitrd.com"
     ], # Añadida la URL de producción de Vercel
     allow_credentials=True,
     allow_methods=["*"],
